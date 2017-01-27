e0ee9229679b19bec86d565f918b9ab8f5822b7b
Elaine Yeung <yeung.egs@gmail.com>
2017-01-27 11:46:36 -0800

    initial project version

---
+++ ch3-ex4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+  int sum;
+
+  sum = -15 + 87;
+  printf ("The difference of 87 and 15 is %i\n",sum);
+
+  return 0;
+}
---
+++ ch4-ex4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+  float C;
+  int F=27;
+
+  C=(F-32)/1.8;
+  printf("%i degrees F converts to %f degrees C\n",F,C);
+
+  return 0;
+}
