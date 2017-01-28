4a04e13c58e40ad8123a41fc3c2d8d9c81c9c61f
Elaine Yeung <yeung.egs@gmail.com>
2017-01-27 16:21:52 -0800

    Chapter 3 exercises

---
+++ ch3/ch3-ex2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (void)
+{
+  printf ("1. In C, lowercase letters are significant.\n2. main is where program execution begins.\n3. Opening and closing braces enclose program statements in a routine.\n4. All program statements must be terminated by a semicolon.\n");
+
+  return 0;
+}
