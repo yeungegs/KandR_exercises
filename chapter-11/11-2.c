7be09b62bb704326488c9d885f65c3ae4614ccf5
Elaine Yeung <yeung.egs@gmail.com>
2017-03-30 22:17:50 +0000

    Add exercise 11-2

--- chapter-11/11-2.c
+++ chapter-11/11-2.c
@@ -1,12 +1,4 @@
-/* program to demonstrate the bitwise AND operator
-Truth Table for result of b1 ANDed with b2 for all possible values of b1, b2
-b1  b2  b1 & b2
------------------
-0   0      0
-0   1      0
-0   0      0
-1   1      1
- */
+/* program to illustrate bitwise operators */
 
 #include <stdio.h>
 int main (void)
@@ -15,43 +7,25 @@ int main (void)
    word3 are given octal values of 077, 0150, and 0210 */
 /* if u or U follows an integer constant, it is treated as unsigned */
 
-	unsigned int word1 = 077u;
-	unsigned int word2 = 0150u;
-	unsigned int word3 = 0210u;
+	unsigned int w1 = 0525u;
+	unsigned int w2 = 0707u;
+	unsigned int w3 = 0122u;
 
-/* first printf call displays octal 50 as result of bitwise
-ANDing word1 with word2. This depicts how the value was calculated:
-word1   ... 000  111  111      077
-word2   ... 001  101  000   & 0150
-       ----------------------------
-            000  101  000      050
-*/
-	printf("%o ", word1 & word2);
+	printf("1. w1 & w2, w1 | w2, w1 ^ w3\n%o %o %o\n\n", w1 & w2, w1 | w2, w1 ^ w3);
+	printf("2. ~w1, ~w2, ~w3\n%o %o %o\n\n", ~w1, ~w2, ~w3);
+	printf("3. w1 ^ w1, w1 & ~w2, w1 | w2 | w3\n%o %o %o\n\n", w1 ^ w1, w1 & ~w2, w1 | w2 | w3);
+/**
+ * for the following printf() call it is important to remember that the bitwise
+ *AND operator has higher precedence than the bitwise OR, because this fact
+ *influences the resulting value of the expression
+ */
+	printf("4. w1 | w2 & w3, w1 | w2 & ~w3\n%o %o\n\n", w1 | w2 & w3, w1 | w2 & ~w3);
+	printf("5. ~(~w1 & ~w2), ~(~w1 | ~w2))\n%o %o\n\n", ~(~w1 & ~w2), ~(~w1 | ~w2));
 
-/* next printf call dxplays octal 77 which is result of ANDing word1 with word1
-   by definition, any quantity x, when ANDed with itself, produces x */
-
-	printf("%o ", word1 & word1);
-
-/* third printf call displays 10 - the result of ANDing word1, word2 and word3
-bitwise ANDing evaluates from left to right
-word1   ... 000  111  111      077
-word2   ... 001  101  000   & 0150
-word3   ... 010  001  000   & 0210
-       ----------------------------
-            000  001  000      010
-*/
-	printf("%o ", word1 & word2 & word3);
-
-/* final printf call extracts rightmost bit of word1.
-This is another way of testing if an integer is even or odd because
-that rightmost bit of any odd integer is 1 and of any even integer is 0
-word1   ... 000  111  111      077
-        ... 000  000  001    &   1
------------------------------------
-            000  000  001        1
-*/
-	printf("%o\n", word1 & 1);
+	w1 ^= w2;
+	w2 ^= w1;
+	w1 ^= w2;
+	printf("w1 ^= w2;\nw2 ^= w1;\nw1 ^= w2;\nw1 = %o, w2 = %o\n", w1, w2);
 
 	return (0);
 }
